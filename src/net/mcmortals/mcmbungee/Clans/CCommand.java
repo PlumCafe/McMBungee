405d737818f6cfb1f2a1e9e80b71aafe43d15886
Bocktrow <spyrospagk@gmail.com>
2014-03-14 21:13:52 +0200

    Changes Whatever

--- src/net/mcmortals/mcmbungee/Clans/CCommand.java
+++ src/net/mcmortals/mcmbungee/Clans/CCommand.java
@@ -3,6 +3,7 @@ package net.mcmortals.mcmbungee.Clans;
 import net.mcmortals.mcmbungee.main;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -28,7 +29,7 @@ public class CCommand extends Command {
                     if (!isInAClan((ProxiedPlayer) sender)) {
                         try {
                             Statement s = this.m.connect.createStatement();
-                            String jc = randomString(8);
+                            String jc = randomString(5);
                             s.executeUpdate("INSERT INTO McMClan (Name, Owner, JoinCode) VALUES ('" + args[1] +"', '" + sender.getName() +"', '" + jc + "')");
                             ResultSet res = s.executeQuery("SELECT * FROM McMClan WHERE Name='" + args[1] + "'");
                             if (res.next()) {
@@ -48,19 +49,22 @@ public class CCommand extends Command {
                             Statement s = this.m.connect.createStatement();
                             ResultSet res = s.executeQuery("SELECT * FROM McMClan WHERE ID='" + getClan((ProxiedPlayer) sender) + "'");
                             if (res.next()) {
+                                String own = res.getString("Owner");
+                                String jc = res.getString("JoinCode");
                                 sender.sendMessage(prefix().append(" == Clan information == ").color(ChatColor.GOLD).bold(true).create());
                                 sender.sendMessage(prefix().append("Clan name: ").color(ChatColor.AQUA).append(res.getString("Name")).color(ChatColor.GREEN).bold(true).create());
-                                sender.sendMessage(prefix().append("Clan owner: ").color(ChatColor.AQUA).append(res.getString("Owner")).color(ChatColor.GREEN).create());
+                                sender.sendMessage(prefix().append("Clan owner: ").color(ChatColor.AQUA).append(own).color(ChatColor.GREEN).create());
                                 sender.sendMessage(prefix().append("Clan members: ").color(ChatColor.AQUA).create());
                                 ResultSet re = s.executeQuery("SELECT * FROM McMPData WHERE ClanID='" + getClan((ProxiedPlayer) sender) + "'");
-                                while (re.next()) { sender.sendMessage(prefix().append("> ").color(ChatColor.GOLD).bold(true).append(re.getString("PlayerName")).color(ChatColor.GREEN).bold(false).create()); }
-                                if (res.getString("Owner").equals(sender.getName())) {
+                                try {while (re.next()) { sender.sendMessage(prefix().append("> ").color(ChatColor.GOLD).bold(true).append(re.getString("PlayerName")).color(ChatColor.GREEN).bold(false).create()); } } catch (Exception ex) {}
+                                if (own.equals(sender.getName())) {
                                     sender.sendMessage(prefix().append(" ====== Owner info ====== ").color(ChatColor.RED).bold(false).create());
-                                    sender.sendMessage(prefix().append("Join code: ").color(ChatColor.AQUA).append(res.getString("JoinCode")).color(ChatColor.GREEN).create());
+                                    sender.sendMessage(prefix().append("Join code: ").color(ChatColor.AQUA).append(jc).color(ChatColor.GREEN).create());
                                 }
                                 sender.sendMessage(prefix().append(" =================== ").color(ChatColor.GOLD).bold(true).create());
                             }
-                        } catch (Exception ex) {}
+                        } catch (Exception ex) {
+                            ex.printStackTrace();}
                     } else sender.sendMessage(prefix().append("You are not in a clan!").color(ChatColor.RED).create());
                 } else sender.sendMessage(prefix().append("Usage: /c info").color(ChatColor.RED).create());
             // DISBAND COMMAND :@
@@ -76,9 +80,10 @@ public class CCommand extends Command {
                                 String name = res.getString("Name");
                                 res.close();
                                 if (own.equalsIgnoreCase(sender.getName())) {
+                                    sendClanMessage(id, ChatColor.GREEN + sender.getName() + " disbanded the clan!");
                                     s.executeUpdate("DELETE FROM McMClan WHERE ID=" + id);
                                     s.executeUpdate("UPDATE McMPData SET ClanID=-1 WHERE ClanID=" + id);
-                                    sender.sendMessage(prefix().append("Clan ").color(ChatColor.GOLD).append(name).color(ChatColor.GOLD).bold(true).append(" deleted successfully!").color(ChatColor.GOLD).bold(false).create());
+                                    sender.sendMessage(prefix().append("Clan ").color(ChatColor.GOLD).append(name).color(ChatColor.GOLD).bold(true).append(" got disbanded successfully!").color(ChatColor.GOLD).bold(false).create());
                                 } else sender.sendMessage(prefix().append("You are not the owner of your clan!").color(ChatColor.RED).create());
                             } else sender.sendMessage(prefix().append("Some error occurred! Report this to staff immediately!").color(ChatColor.RED).create());
                         } catch (Exception ex) {ex.printStackTrace();}
@@ -96,6 +101,7 @@ public class CCommand extends Command {
                                 String name = res.getString("Name");
                                 res.close();
                                 if (!own.equalsIgnoreCase(sender.getName())) {
+                                    sendClanMessage(res.getInt("ID"), ChatColor.GREEN + sender.getName() + " left the clan!");
                                     s.executeUpdate("UPDATE McMPData SET ClanID=-1 WHERE PlayerName='" + sender.getName() + "'");
                                     sender.sendMessage(prefix().append("Left clan ").color(ChatColor.GOLD).append(name).color(ChatColor.GOLD).bold(true).append(" successfully!").color(ChatColor.GOLD).bold(false).create());
                                 } else sender.sendMessage(prefix().append("You cannot leave your own clan! Disband it instead with /c disband or transfer it by using /c transfer [Player].").color(ChatColor.RED).create());
@@ -120,6 +126,7 @@ public class CCommand extends Command {
                                     if (re.getInt("ClanID")!=id ) { sender.sendMessage(prefix().append("You can only transfer the clan to a member of your clan!").color(ChatColor.RED).create()); return;}
                                 } else {sender.sendMessage(prefix().append("You can only transfer the clan to a member of your clan!").color(ChatColor.RED).create()); return;}
                                 if (own.equalsIgnoreCase(sender.getName())) {
+                                    sendClanMessage(id, ChatColor.GREEN + args[1] + " is now the clan owner!");
                                     s.executeUpdate("UPDATE McMClan SET Owner='" + args[1] +"' WHERE ID=" + id );
                                     sender.sendMessage(prefix().append("Transferred clan ").color(ChatColor.GOLD).append(name).color(ChatColor.GOLD).bold(true).append(" to " + args[1] +" successfully!").color(ChatColor.GOLD).bold(false).create());
                                 } else sender.sendMessage(prefix().append("You are not the owner of your clan!").color(ChatColor.RED).create());
@@ -137,8 +144,9 @@ public class CCommand extends Command {
                             if (res.next()) {
                                 if (res.getInt("ClanID")==-1) {
                                     ResultSet re = s.executeQuery("SELECT * FROM McMClan WHERE JoinCode='" + args[1] + "'");
-                                    String m= re.getString("Name");
                                     if (re.next()){
+                                        sendClanMessage(re.getInt("ID"), ChatColor.GREEN + sender.getName() + " joined the clan!");
+                                        String m= re.getString("Name");
                                         s.executeUpdate("UPDATE McMPData SET ClanID=" + re.getInt("ID") + " WHERE PlayerName='" + sender.getName() + "'");
                                         sender.sendMessage(prefix().append("Joined clan ").color(ChatColor.GOLD).append(m).color(ChatColor.GOLD).bold(true).append(" successfully!").color(ChatColor.GOLD).bold(false).create());
                                     } else sender.sendMessage(prefix().append("Invalid join code!").color(ChatColor.RED).create());
@@ -180,11 +188,23 @@ public class CCommand extends Command {
     }
 
     String randomString(final int len) {
-        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String AB = "0123456789";
         Random rnd = new Random();
         StringBuilder sb = new StringBuilder( len );
         for( int i = 0; i < len; i++ )
             sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
         return sb.toString();
+    }
+
+    void sendClanMessage(Integer cid, String msg) {
+        try {
+            Statement statement = this.m.connect.createStatement();
+            ResultSet res = statement.executeQuery("SELECT * FROM McMPData WHERE ClanID=" + cid);
+            while (res.next()) {
+                try {
+                    ProxyServer.getInstance().getPlayer(res.getString("PlayerName")).sendMessage(prefix().append(msg).create());
+                } catch (Exception ex) {}
+            }
+        } catch (Exception ex) {}
     }
 }

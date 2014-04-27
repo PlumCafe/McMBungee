28661a7dca4f2a78df022f971fa3b8bf41967545
Red_Epicness <redepicness1@gmail.com>
2014-04-27 07:15:28 +0200

    /nlookup

--- src/net/mcmortals/mcmbungee/Commands/NewLookupCommand.java
+++ src/net/mcmortals/mcmbungee/Commands/NewLookupCommand.java
@@ -10,26 +10,30 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
 
-public class LookupCommand extends Command {
+public class NewLookupCommand extends Command {
 
     main m = null;
 
-    public LookupCommand(main This) {
-        super("lookup", "","lp");
+    public NewLookupCommand(main This) {
+        super("nlookup", "");
         m = This;
     }
 
     public void execute(CommandSender sender, String[] args) {
-        if (m.hasPermission(sender,6)) {
+        if (m.hasPermission(sender, 6)) {
             if (args.length==1) {
                 try {
                     Statement s = m.connect.createStatement();
                     ResultSet res = s.executeQuery("SELECT * FROM McMPData WHERE PlayerName='" + args[0] +"'") ;
                     if (res.next()) {
-                        sender.sendMessage("§4[§cMcM§4] §6§lPlayer lookup: §f" + getPlayerName(res.getString("PlayerName"),ChatColor.WHITE,res.getInt("Rank")));
-                        sender.sendMessage("§6Rank: " + McMCommand.getRank(res.getInt("Rank")));
-                        sender.sendMessage("§6Tokens: §b" + res.getInt("Tokens"));
-                        sender.sendMessage("§6Tournament points: §b" + res.getInt("TournPoints"));
+                        sender.sendMessage(appendName(prefix().append("Player lookup:").color(ChatColor.GOLD).bold(true), res.getInt("Rank"), res.getString("PlayerName")).create());
+                        //sender.sendMessage("§4[§cMcM§4] §6§lPlayer lookup: §f" + getPlayerName(res.getString("PlayerName"),ChatColor.WHITE,res.getInt("Rank")));
+                        sender.sendMessage(new ComponentBuilder("Rank: ").color(ChatColor.GOLD).append(McMCommand.getRank(res.getInt("Rank"))).create());
+                        //sender.sendMessage("§6Rank: " + McMCommand.getRank(res.getInt("Rank")));
+                        sender.sendMessage(new ComponentBuilder("Tokens: ").color(ChatColor.GOLD).append(""+res.getInt("Tokens")).color(ChatColor.AQUA).create());
+                        //sender.sendMessage("§6Tokens: §b" + res.getInt("Tokens"));
+                        sender.sendMessage(new ComponentBuilder("Tournament points:").color(ChatColor.GOLD).append(""+res.getInt("TournPoints")).color(ChatColor.AQUA).create());
+                        //sender.sendMessage("§6Tournament points: §b" + res.getInt("TournPoints"));
                         Date fsl = new Date(res.getLong("FirstLogin"));
                         Date lsl = new Date(res.getLong("LastLogin"));
                         if (!fsl.toGMTString().contains("1970")) {
@@ -67,40 +71,21 @@ public class LookupCommand extends Command {
         return new ComponentBuilder("[").color(ChatColor.DARK_RED).append("McM").color(ChatColor.RED).append("] ").color(ChatColor.DARK_RED);
     }
 
-    public static String getPlayerName(String name, ChatColor cc, Integer rank) {
-        try {
-            if (rank==10) {
-                return "§4§l[Op] §b"+ name + "§r";
-            }
-            if (rank==9) {
-                return "§5§l[Dev] §b" + name + "§r";
-            }
-            if (rank==8) {
-                return "§c§l[Admin] §b" + name + "§r";
-            }
-            if (rank==7) {
-                return "§2[Mod] §f" + cc + name + "§r";
-            }
-            if (rank==6) {
-                return "§9[Helper] §f" + cc  + name + "§r";
-            }
-            if (rank==5) {
-                return "§2[Host] §f" + cc  + name + "§r";
-            }
-            if (rank==4) {
-                return "§3[Builder] §f" + cc  + name + "§r";
-            }
-            if (rank==3) {
-                return "§4[Legend] §f" + cc  + name + "§r";
-            }
-            if (rank==2) {
-                return "§6[YT] §f" + cc  + name + "§r";
-            }
-            if (rank==1) {
-                return "§a[VIP] §f" + cc  + name + "§r";
-            }
-        } catch (Exception ex) {}
-        return "§f" + cc  + name + "§r";
+    public static ComponentBuilder appendName(ComponentBuilder b, int rank, String name){
+        switch (rank){
+            case (10): return b.append("[Op] ").color(ChatColor.DARK_RED).bold(true).append(name).color(ChatColor.AQUA); break;
+            case (9): return b.append("[Dev] ").color(ChatColor.DARK_PURPLE).bold(true).append(name).color(ChatColor.AQUA); break;
+            case (8): return b.append("[Admin] ").color(ChatColor.RED).bold(true).append(name).color(ChatColor.AQUA); break;
+            case (7): return b.append("[Mod] ").color(ChatColor.DARK_GREEN).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (6): return b.append("[Helper] ").color(ChatColor.BLUE).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (5): return b.append("[Host] ").color(ChatColor.DARK_GREEN).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (4): return b.append("[Builder] ").color(ChatColor.DARK_AQUA).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (3): return b.append("[Legend] ").color(ChatColor.DARK_RED).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (2): return b.append("[YT] ").color(ChatColor.GOLD).bold(true).append(name).color(ChatColor.WHITE); break;
+            case (1): return b.append("[VIP] ").color(ChatColor.GREEN).bold(true).append(name).color(ChatColor.WHITE); break;
+            default: return b.append(name).color(ChatColor.WHITE); break;
+        }
+        return null;
     }
 
     public String yesorno(int a) {
@@ -108,4 +93,3 @@ public class LookupCommand extends Command {
         return "No";
     }
 }
-

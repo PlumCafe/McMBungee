bbf1e188be6d1d8fdebf791cdf71ca295f79d8eb
Red_Epicness <redepicness1@gmail.com>
2014-05-04 01:30:49 +0200

    Reorganizing #4 (Kick.java)

---
+++ src/net/mcmortals/mcmbungee/Commands/Kick.java
@@ -0,0 +1,63 @@
+package net.mcmortals.mcmbungee.Commands;
+
+import net.mcmortals.mcmbungee.Utility.Utility;
+import net.mcmortals.mcmbungee.main;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kick
+        extends Command {
+    private main m = new main();
+
+    public Kick(main This) {
+        super("kick", "");
+        this.m = This;
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        //Permissions Check
+        if (m.hasPermission(sender,6)) {
+            sender.sendMessage(Utility.prefix().append("You cannot do that!").color(ChatColor.RED).create());
+            return;
+        }
+        //Argument Length Check
+        if (args.length>=2) {
+            sender.sendMessage(Utility.prefix().append("Usage: §b/kick [Player] [Reason]").color(ChatColor.RED).create());
+            return;
+        }
+        String msg = "";
+        try {
+            //Combine Message
+            int w = 1;
+            do {
+                msg = msg + " " + args[w];
+                w++;
+            } while (w < args.length);
+            //Kick Player
+            ProxiedPlayer p = ProxyServer.getInstance().getPlayer(args[0]);
+            p.disconnect(new ComponentBuilder("You have been kicked from the server!").color(ChatColor.RED).append("\n")
+                    .append("Reason:").color(ChatColor.RED).append(msg).color(ChatColor.GOLD).create());
+        } catch (Exception e){
+            //Report If Target Is Offline
+            e.printStackTrace();
+            sender.sendMessage(new ComponentBuilder("That player is not online.").color(ChatColor.RED).create());
+        }
+        try {
+            //Register an Infraction
+            Statement statement = m.connect.createStatement();
+            statement.executeUpdate("INSERT INTO McMInfractions (PlayerName, Enforcer, Type, Reason) VALUES " +
+                    "('" + args[0] + "', '" + sender.getName() + "', '" + "Kick" + "', '" + msg + "')");
+            //Alert Staff
+            m.sendToStaff(ChatColor.AQUA + sender.getName() + " kicked " + args[0] + " for " + msg + ".");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- src/net/mcmortals/mcmbungee/main.java
+++ src/net/mcmortals/mcmbungee/main.java
@@ -47,7 +47,7 @@ public class main extends Plugin implements Listener {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new CCommand(this));
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Ban());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new UnbanCommand(this));
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new KickCommand(this));
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Kick(this));
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new SCommand(this));
         //ProxyServer.getInstance().getPluginManager().registerCommand(this, new LookupCommand(this));
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new TempbanCommand(this));
